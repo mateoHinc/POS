5a599f9483ec44b663f9e4f3368fa1e4e8c23c99
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2021-07-01 15:31:34 -0500

    Basic' project

---
+++ POS/POS/POS.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace POS.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new POS.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ POS/POS/POS.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace POS.Skia.Gtk
+{
+}
